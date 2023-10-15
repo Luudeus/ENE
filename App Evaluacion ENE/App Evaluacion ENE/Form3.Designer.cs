e211433368014153a8b94960cc4a27f4b031f20c
Luu <lacabrales@gmail.com>
2023-10-15 20:21:11 -0300

    Se agregó la tabla en el form3 y las consultas mediante el botón de selección.

--- App Evaluacion ENE/App Evaluacion ENE/Form3.Designer.cs
+++ App Evaluacion ENE/App Evaluacion ENE/Form3.Designer.cs
@@ -28,42 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.trabajadoresLabel = new System.Windows.Forms.Label();
             this.trabajadoresBtn = new System.Windows.Forms.Button();
+            this.workerDgv = new System.Windows.Forms.DataGridView();
+            this.contextMenuStripWorker = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tODOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.workerDgv)).BeginInit();
+            this.contextMenuStripWorker.SuspendLayout();
             this.SuspendLayout();
             // 
             // trabajadoresLabel
             // 
             this.trabajadoresLabel.AutoSize = true;
             this.trabajadoresLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.trabajadoresLabel.Location = new System.Drawing.Point(84, 57);
+            this.trabajadoresLabel.Location = new System.Drawing.Point(162, 34);
             this.trabajadoresLabel.Name = "trabajadoresLabel";
-            this.trabajadoresLabel.Size = new System.Drawing.Size(174, 25);
+            this.trabajadoresLabel.Size = new System.Drawing.Size(153, 25);
             this.trabajadoresLabel.TabIndex = 0;
-            this.trabajadoresLabel.Text = "TRABAJADORES";
+            this.trabajadoresLabel.Text = "TRABAJADOR:";
             // 
             // trabajadoresBtn
             // 
             this.trabajadoresBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.trabajadoresBtn.Image = global::App_Evaluacion_ENE.Properties.Resources._32195__1_;
             this.trabajadoresBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.trabajadoresBtn.Location = new System.Drawing.Point(282, 53);
+            this.trabajadoresBtn.Location = new System.Drawing.Point(349, 30);
             this.trabajadoresBtn.Name = "trabajadoresBtn";
             this.trabajadoresBtn.Size = new System.Drawing.Size(255, 33);
             this.trabajadoresBtn.TabIndex = 1;
             this.trabajadoresBtn.Text = "TODOS";
             this.trabajadoresBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.trabajadoresBtn.UseVisualStyleBackColor = true;
+            this.trabajadoresBtn.Click += new System.EventHandler(this.trabajadoresBtn_Click);
+            // 
+            // workerDgv
+            // 
+            this.workerDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.workerDgv.Location = new System.Drawing.Point(12, 90);
+            this.workerDgv.Name = "workerDgv";
+            this.workerDgv.RowHeadersWidth = 51;
+            this.workerDgv.RowTemplate.Height = 24;
+            this.workerDgv.Size = new System.Drawing.Size(941, 326);
+            this.workerDgv.TabIndex = 2;
+            // 
+            // contextMenuStripWorker
+            // 
+            this.contextMenuStripWorker.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStripWorker.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tODOSToolStripMenuItem});
+            this.contextMenuStripWorker.Name = "contextMenuStripWorker";
+            this.contextMenuStripWorker.Size = new System.Drawing.Size(127, 28);
+            // 
+            // tODOSToolStripMenuItem
+            // 
+            this.tODOSToolStripMenuItem.Name = "tODOSToolStripMenuItem";
+            this.tODOSToolStripMenuItem.Size = new System.Drawing.Size(126, 24);
+            this.tODOSToolStripMenuItem.Text = "TODOS";
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(965, 547);
+            this.Controls.Add(this.workerDgv);
             this.Controls.Add(this.trabajadoresBtn);
             this.Controls.Add(this.trabajadoresLabel);
             this.Name = "Form3";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.workerDgv)).EndInit();
+            this.contextMenuStripWorker.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,5 +107,8 @@
 
         private System.Windows.Forms.Label trabajadoresLabel;
         private System.Windows.Forms.Button trabajadoresBtn;
+        private System.Windows.Forms.DataGridView workerDgv;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripWorker;
+        private System.Windows.Forms.ToolStripMenuItem tODOSToolStripMenuItem;
     }
 }
--- App Evaluacion ENE/App Evaluacion ENE/Form3.cs
+++ App Evaluacion ENE/App Evaluacion ENE/Form3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace App_Evaluacion_ENE
 {
@@ -15,6 +16,134 @@ namespace App_Evaluacion_ENE
         public Form3()
         {
             InitializeComponent();
+            this.Load += Form3_Load;
+            trabajadoresBtn.TextChanged += trabajadoresBtn_TextChanged;
+            contextMenuStripWorker.ItemClicked += new ToolStripItemClickedEventHandler(contextMenuStripWorker_ItemClicked);
+
         }
+
+        private void LlenarDataGridView(string clavePrimaria = null)
+        {
+            try
+            {
+                using (var db = new Database())
+                {
+                    db.OpenConnection();
+
+                    string query;
+                    MySqlCommand cmd = new MySqlCommand();
+
+                    if (string.IsNullOrEmpty(clavePrimaria) || clavePrimaria == "TODOS")
+                    {
+                        // Si no hay filtro o el filtro es "TODOS", seleccionamos todos los empleados.
+                        query = "SELECT Rut_Empleado, Nombre, Direccion, Telefono, Sueldo_Liquido, Sueldo_Bruto FROM Empleados";
+                    }
+                    else
+                    {
+                        // Si hay un filtro, seleccionamos los empleados que coincidan con la clave primaria.
+                        query = "SELECT Rut_Empleado, Nombre, Direccion, Telefono, Sueldo_Liquido, Sueldo_Bruto FROM Empleados WHERE Rut_Empleado = @rut";
+                        cmd.Parameters.AddWithValue("@rut", clavePrimaria); // Buscar empleados que coincidan con la clave primaria
+                    }
+
+                    cmd.Connection = db.GetConnection();
+                    cmd.CommandText = query;
+
+                    DataTable dataTable = new DataTable();
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    da.Fill(dataTable);
+
+                    // Asignar los datos al DataGridView
+                    workerDgv.DataSource = dataTable;
+
+                    // Ajustar el modo de las columnas para que no queden espacios a los lados
+                    foreach (DataGridViewColumn column in workerDgv.Columns)
+                    {
+                        column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    }
+
+                    // Calcular cuántas filas en blanco se necesitan para que no queden espacios abajo del dgv
+                    int heightOfOneRow = workerDgv.RowTemplate.Height; // Altura de una fila
+                    int totalVisibleHeight = workerDgv.Height; // Altura total del DataGridView
+                    int currentRowsHeight = workerDgv.Rows.Count * heightOfOneRow; // Altura total de las filas actuales
+
+                    // Si la altura total de las filas actuales es menor que la altura visible, se agregan filas en blanco
+                    while (currentRowsHeight < totalVisibleHeight)
+                    {
+                        dataTable.Rows.Add(); // Agregar una fila en blanco al final
+                        currentRowsHeight += heightOfOneRow; // Actualizar la altura total de las filas
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener datos: " + ex.Message);
+            }
+        }
+
+
+        private List<string> ObtenerRutsEmpleados()
+        {
+            List<string> ruts = new List<string>();
+
+            using (var db = new Database())
+            {
+                db.OpenConnection();
+
+                string query = "SELECT Rut_Empleado FROM Empleados";
+                MySqlCommand cmd = new MySqlCommand(query, db.GetConnection());
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ruts.Add(reader["Rut_Empleado"].ToString());
+                }
+
+                reader.Close();
+                db.CloseConnection();
+            }
+
+            return ruts;
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // Obtener los RUTs de los empleados de la base de datos
+            List<string> rutsEmpleados = ObtenerRutsEmpleados();
+
+            // Agregar un elemento al menú para cada RUT
+            foreach (string rut in rutsEmpleados)
+            {
+                ToolStripItem menuItem = new ToolStripMenuItem(rut);
+                menuItem.Click += (senderItem, eventArgs) => // Cambié 'sender' y 'e' a 'senderItem' y 'eventArgs'
+                {
+                    // Aquí puedes manejar el evento click del elemento, si es necesario.
+                    // Por ejemplo, podrías querer cargar detalles del empleado cuando se haga clic en su RUT.
+                };
+                contextMenuStripWorker.Items.Add(menuItem);
+            }
+
+            // Cuando el formulario se carga, se llena el dgv asumiendo que 'trabajadoresBtn' tiene el texto "TODOS" al inicio.
+
+            LlenarDataGridView(trabajadoresBtn.Text);
+        }
+
+
+        private void trabajadoresBtn_TextChanged(object sender, EventArgs e)
+        {
+            // Cada vez que el texto cambia, actualizamos el DataGridView.
+            LlenarDataGridView(trabajadoresBtn.Text);
+        }
+
+        private void trabajadoresBtn_Click(object sender, EventArgs e)
+        {
+            contextMenuStripWorker.Show(trabajadoresBtn, new Point(0, trabajadoresBtn.Height));
+        }
+        private void contextMenuStripWorker_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            trabajadoresBtn.Text = e.ClickedItem.Text;
+            contextMenuStripWorker.Close();
+        }
+
     }
 }
