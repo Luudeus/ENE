4cce0276b48cfdc18ea8a1d68e7deca90b26a597
Luu <lacabrales@gmail.com>
2023-10-15 17:34:38 -0300

    Se agrego una clase de Base de datos con los bases de mi db local

---
+++ App Evaluacion ENE/App Evaluacion ENE/Database.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace App_Evaluacion_ENE
+{
+    public class Database : IDisposable
+    {
+        private readonly MySqlConnection _connection;
+
+        public Database()
+        {
+            string server = "localhost";
+            string database = "ENE";
+            string username = "root";
+            string password = "pfxj7KtKxrjf";
+            string port = "3306";
+
+            string connectionString = $"Server={server};Database={database};User ID={username};Password={password};Port={port};";
+            _connection = new MySqlConnection(connectionString);
+        }
+
+        public void OpenConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return _connection;
+        }
+
+        public void Dispose()
+        {
+            CloseConnection();
+        }
+    }
+}
